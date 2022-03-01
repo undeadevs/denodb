2f6a6549cc9966f9d56b265a233da2df62a43ff4
undeadevs <42311213+undeadevs@users.noreply.github.com>
2022-03-02 04:32:28 +0800

    Update mysql-connector.ts

--- lib/connectors/mysql-connector.ts
+++ lib/connectors/mysql-connector.ts
@@ -78,20 +78,19 @@ export class MySQLConnector implements Connector {
       : "execute";
 
     for (let i = 0; i < subqueries.length; i++) {
-      const result = await queryClient[queryMethod](subqueries[i]);
-//       await this.reconnect();
-//       let result = null;
-//       try {
-//         result = await queryClient[queryMethod](subqueries[i]);
-//       } catch (error) {
-//         //reconnect client on timeout error
-//         if (error instanceof MySQLReponseTimeoutError) {
-//           await this.reconnect();
-//           return this.query(queryDescription, client);
-//         }
+//       const result = await queryClient[queryMethod](subqueries[i]);
+      let result = null;
+      try {
+        result = await queryClient[queryMethod](subqueries[i]);
+      } catch (error) {
+        //reconnect client on timeout error
+        if (error instanceof MySQLReponseTimeoutError) {
+          await this.close();
+          return this.query(queryDescription, client);
+        }
 
-//         throw error;
-//       }
+        throw error;
+      }
       
       if (i === subqueries.length - 1) {
         return result;
